f16c94b1da653d925f4e2e972d68c461221c21e8
Shreyas-Yadav <shreyasyadav11@gmail.com>
2024-12-26 12:10:44 -0800

    removed testing code

--- App/api/client.js
+++ App/api/client.js
@@ -5,33 +5,4 @@ const apiClient = create({
   timeout: 7000,
 });
 
-// apiClient.addRequestTransform((request) => {
-//   console.log("Request: ", {
-//     url: request.url,
-//     method: request.method,
-//     data: request.data,
-//     headers: request.headers,
-//   });
-
-//   if (request.data instanceof FormData) {
-//     request.headers["Content-Type"] = "multipart/form-data";
-
-//     const formDataEntries = [];
-//     for (let pair of request.data.entries()) {
-//       formDataEntries.push(pair);
-//     }
-//     console.log("FormData entries:", formDataEntries);
-//   }
-// });
-
-// apiClient.addMonitor((response) => {
-//   console.log("Response:", {
-//     url: response.config.url,
-//     status: response.status,
-//     problem: response.problem,
-//     data: response.data,
-//     duration: response.duration,
-//   });
-// });
-
 export default apiClient;
